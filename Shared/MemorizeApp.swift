a71ec97203fe57ace1c7c9c8781d31fc26be3c70
Alliston <alliston@outlook.com>
2021-03-11 12:55:29 -0300

    Project Creation

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Alliston Aleixo on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
