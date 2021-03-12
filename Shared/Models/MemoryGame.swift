9160db23f6d095971ce7342489930d4028577d73
Alliston <alliston@outlook.com>
2021-03-12 10:17:51 -0300

    Lecture 2 - MVVM and the Swift Type System

---
+++ Shared/Models/MemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Alliston Aleixo on 11/03/21.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    init(numbersOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numbersOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex * 2, content: content))
+            cards.append(Card(id: pairIndex * 2 + 1, content: content))
+        }
+    }
+    
+    func choose(card: Card) {
+        print("Card chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var id: Int
+        
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
---
+++ Shared/VIewModels/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Alliston Aleixo on 11/03/21.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = [ "👻", "🎃", "🕷" ]
+        
+        return MemoryGame<String>(numbersOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intents
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
